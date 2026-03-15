fd93cbe75c1662dfe787ae212f8ca584ac3cf289
uguecker <guecker@gmx.de>
2026-03-15 18:55:16 +0100

    Einrichten der virtual environment; Kapitel _01_Einstieg

---
+++ _01_Einstieg/hello_world.py
@@ -0,0 +1,7 @@
+print("Hello World")
+x = 10
+print(x)
+import math
+
+phi = math.pi / 2
+print(math.sin(phi))
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from datenanalyse-mit-python-ss2026!")
+
+
+if __name__ == "__main__":
+    main()
