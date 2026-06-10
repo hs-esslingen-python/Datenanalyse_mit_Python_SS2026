b41697227927b9333f036e61cddeb7a672484fe7
uguecker <guecker@gmx.de>
2026-06-10 22:27:46 +0200

    nach Vorlesung am 10.6.2026

--- _30_Verschiedenes/Python_Module/Modul1.py
+++ _30_Verschiedenes/Python_Module/Modul1.py
@@ -11,5 +11,6 @@ import numpy as np
 
 
 def function_RMSE(y, y_pred):
+    print(f"Funktion function_RMSE() aus Modul1 wurde aufgerufen. {__name__}")
     RMSE = np.sqrt(np.mean((y-y_pred)**2))
     return RMSE
